bd3054e0a420cb4ca9c25085c4494f3467c76ad4
HaLam <itzlamz9x@gmail.com>
2017-02-04 15:57:20 +0700

    Init row + assets

---
+++ 4217/MonaCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MonaCollectionViewCell.swift
+//  4217
+//
+//  Created by Ha Lam on 2/4/17.
+//  Copyright © 2017 MarInc. All rights reserved.
+//
+
+import UIKit
+
+class MonaCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpView() {
+        backgroundColor = .orange
+    }
+    
+}
--- 4217/MonaCollectionViewController.swift
+++ 4217/MonaCollectionViewController.swift
@@ -15,51 +15,28 @@ class MonaCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.register(MonaCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        //setup after loading the view.
+        collectionView?.alwaysBounceVertical = true
         navigationItem.title = "Siclo application"
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return 3
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
+        return collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//    
+//        // Configure the cell
+//    
+//        return cell
     }
 
     // MARK: UICollectionViewDelegate
@@ -94,3 +71,24 @@ class MonaCollectionViewController: UICollectionViewController {
     */
 
 }
+
+extension MonaCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
