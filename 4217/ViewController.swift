ba784f3435a2d44e8fa3cab689aa09832ce99043
HaLam <itzlamz9x@gmail.com>
2017-02-04 14:56:20 +0700

    Initial Commit

---
+++ 4217/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4217
+//
+//  Created by Ha Lam on 2/4/17.
+//  Copyright © 2017 MarInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
